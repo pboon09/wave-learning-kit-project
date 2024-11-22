13f938bea99b4e5337b61c5b3c6c6c4bc29c56cc
Phakin Boonchanachai <pboonchanachai@gmail.com>
2024-11-22 11:16:32 +0700

    Update read_pin.py

--- read_pin.py
+++ read_pin.py
@@ -1,24 +1,22 @@
 import serial
-import time
-
 arduino_port = "COM7"
-baud_rate = 115200
+baud_rate = 9600
+
+default_data = [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
 
 try:
-    # Establish the serial connection
     ser = serial.Serial(arduino_port, baud_rate, timeout=1)
-    # print(f"Connected to {arduino_port} at {baud_rate} baud.")
-    time.sleep(2)
 
     while True:
         if ser.in_waiting > 0:
-            data = ser.readline().decode('utf-8').strip()
-            print(f"{data}")
-except serial.SerialException as e:
+            raw_data = ser.readline().decode('utf-8').strip()
+            data = list(map(int, raw_data.split()))
+            
+            if data != default_data:
+                print(data)
+
+except Exception as e:
     print(f"Error: {e}")
-except KeyboardInterrupt:
-    print("\nExiting...")
 finally:
     if 'ser' in locals() and ser.is_open:
         ser.close()
-        print("Serial connection closed.")
