bde98dfc6aafec483dc2cdc51d97edbbccaa852b
Phakin Boonchanachai <pboonchanachai@gmail.com>
2024-11-22 18:17:39 +0700

    Update read_pin.py

--- read_pin.py
+++ read_pin.py
@@ -1,22 +1,40 @@
 import serial
+import json
+
 arduino_port = "COM7"
 baud_rate = 9600
 
 default_data = [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
 
+json_data = {
+    "data": default_data
+}
+with open("input.json", 'w') as json_file:
+    json.dump(json_data, json_file, indent=1)
+
 try:
     ser = serial.Serial(arduino_port, baud_rate, timeout=1)
+    print(f"Connected to {arduino_port} at {baud_rate} baud rate.")
 
     while True:
         if ser.in_waiting > 0:
-            raw_data = ser.readline().decode('utf-8').strip()
-            data = list(map(int, raw_data.split()))
-            
-            if data != default_data:
-                print(data)
-
+            try:
+                raw_data = ser.readline().decode('utf-8').strip()
+                data = list(map(int, raw_data.split()))
+                
+                if data != default_data:
+                    json_data = {
+                        "data": data
+                    }
+                    with open("input.json", 'w') as json_file:
+                        json.dump(json_data, json_file, indent=1)
+                    
+                    print("New Data:", data)
+            except ValueError:
+                print("Error parsing incoming data:", raw_data)
 except Exception as e:
     print(f"Error: {e}")
 finally:
     if 'ser' in locals() and ser.is_open:
         ser.close()
+        print("Serial connection closed.")
